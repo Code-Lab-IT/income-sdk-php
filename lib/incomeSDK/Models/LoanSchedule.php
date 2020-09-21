8408a5479811353000c40af4f0c7211445b726c6
odruslan <odruslan@gmail.com>
2020-09-21 17:51:58 +0300

    update loan fields

--- lib/incomeSDK/Models/LoanSchedule.php
+++ lib/incomeSDK/Models/LoanSchedule.php
@@ -5,7 +5,7 @@ namespace incomeSDK\Models;
 use incomeSDK\Core\BaseModel;
 
 /**
- * @property array|null $loan_schedule
+ * @property array|null $schedule_components
  * @property array|null $schedule
  */
 class LoanSchedule extends BaseModel
